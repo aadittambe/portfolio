e8c81898c675e84dfc92525cf3425b1cc10dc00e
Aadit Tambe <aadit.tambe@gmail.com>
2022-06-29 01:35:20 -0400

    prog

--- src/components/Experience.js
+++ src/components/Experience.js
@@ -11,13 +11,19 @@ function Experience() {
             <div className="layout">
                 <div className="col left">
                     {data.map(d => (
-                        <p onMouseEnter={e => (document.getElementById("aadit").src = require(`../images/${d.img}`))}>{d.project}</p>//${d.img}
+                        <div className='story'>
+                            <p>{d.tools}</p>
+                            <img src={require(`../images/tracker.png`)} width="100%"></img>
+                            <p>{d.project}</p>
+                            <p>{d.org}</p>
+                        </div>
                     ))}
                 </div>
                 <div className="col right">
                     <img id="aadit" width={"100%"} src="https://cnsmaryland.org/interactives/fall2021/goucher-poll/illustrations/social-card-image.png" />
                 </div>
             </div>
+            <div className='crap'></div>
         </div>
     )
 }
