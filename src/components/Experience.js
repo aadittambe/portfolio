640eda30670d80950bd0d01f9c41f1f79c739c2a
Aadit Tambe <aadit.tambe@gmail.com>
2022-06-29 22:21:41 -0400

    styles

--- src/components/Experience.js
+++ src/components/Experience.js
@@ -1,31 +1,31 @@
-import React from 'react';
-import '../styles/Experience.css'
-const data = require('../data/stories.json')
+import React from "react";
+import "../styles/Experience.css";
+const data = require("../data/stories.json");
 
 function Experience() {
-    return (
-        <div className='experience'>
-            <h1>
-                Some projects I'm proud of
-            </h1>
-            <div className="layout">
-                <div className="col left">
-                    {data.map(d => (
-                        <div className='story'>
-                            <p>{d.tools}</p>
-                            <img src={require(`../images/tracker.png`)} width="100%"></img>
-                            <p>{d.project}</p>
-                            <p>{d.org}</p>
-                        </div>
-                    ))}
-                </div>
-                <div className="col right">
-                    <img id="aadit" width={"100%"} src="https://cnsmaryland.org/interactives/fall2021/goucher-poll/illustrations/social-card-image.png" />
-                </div>
-            </div>
-            <div className='crap'></div>
-        </div>
-    )
+  return (
+    <div className="experience">
+      <h1>Some projects I'm proud of</h1>
+      <div className="layout">
+        {data.map((d) => (
+          <div className="story">
+            <a href={`../images/tracker.png`}>
+              <p className="tools">{d.tools}</p>
+              <img
+                src={require(`../images/tracker.png`)}
+                alt={d.alt}
+                className="story-img"
+                // width="100%"
+              ></img>
+              <p className="story-name">{d.project}</p>
+              <p className="story-org">{d.org}</p>
+            </a>
+          </div>
+        ))}
+      </div>
+      <div className="crap"></div>
+    </div>
+  );
 }
 
 export default Experience;
