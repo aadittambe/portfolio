5a5e1abb1b321c80a1b7e41c378ea98ef7516aa7
Aadit Tambe <aadit.tambe@gmail.com>
2022-06-29 23:41:56 -0400

    add experience

--- src/components/Experience.js
+++ src/components/Experience.js
@@ -5,26 +5,57 @@ const data = require("../data/stories.json");
 function Experience() {
   return (
     <div className="experience">
-      <h1>Some projects I'm proud of</h1>
+      <h1>Projects and such</h1>
+      <fieldset className="btn-row">
+        <legend>
+          <p> 👀 Looking for a particular type of project? </p>
+        </legend>
+        <p className="filters">
+          <label className="btn reset">
+            <input type="radio" name="storyType" value="column" id="reset" />
+            <span>🌎<br></br>All</span>
+          </label>
+          <label className="btn">
+            <input type="radio" name="storyType" value="text" id="text" />
+            <span>✍️<br></br>Written</span>
+          </label>
+          <label className="btn">
+            <input type="radio" name="storyType" value="data" id="data" />
+            <span>📈<br></br>Data-driven</span>
+          </label>
+          <label className="btn">
+            <input type="radio" name="storyType" value="graphics" id="graphics" />
+            <span>🗺<br></br>Graphics</span>
+          </label>
+          <label className="btn">
+            <input type="radio" name="storyType" value="app" id="app" />
+            <span>🖥<br></br>News app</span>
+          </label>
+          <label className="btn">
+            <input type="radio" name="storyType" value="app" id="docs" />
+            <span>📖<br></br>Docs</span>
+          </label>
+        </p>
+      </fieldset>
       <div className="layout">
-        {data.map((d) => (
-          <div className="story">
+        {data.map((d, i) => (
+          <div className={`story ${d.storyType}`} key={i}>
             <a href={`../images/tracker.png`}>
-              <p className="tools">{d.tools}</p>
+              <p className="story-tools">🧰 {d.tools}</p>
               <img
-                src={require(`../images/tracker.png`)}
+                src={d.img.startsWith("https") ? d.img : require(`../images/${d.img}?.jpeg`)}
                 alt={d.alt}
                 className="story-img"
-                // width="100%"
               ></img>
-              <p className="story-name">{d.project}</p>
-              <p className="story-org">{d.org}</p>
+              <p className="story-org">📍<span className="ital"> {d.org}</span></p>
+              <p>🔗 <span className="story-name">{d.project}</span></p>
+
             </a>
           </div>
         ))}
       </div>
       <div className="crap"></div>
-    </div>
+    </div >
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -1,10 +1,67 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import $ from 'jquery';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
+$(function () {
+  $("#graphics").click(function () {
+    $(".app").hide()
+    $(".data").hide()
+    $(".text").hide()
+    $(".graphics").show()
+    $(".docs").hide()
+    $(".btn.reset span").css("background-color", "#f6f4e6")
+  });
+
+  $("#data").click(function () {
+    $(".app").hide()
+    $(".graphics").hide()
+    $(".text").hide()
+    $(".data").show()
+    $(".docs").hide()
+    $(".btn.reset span").css("background-color", "#f6f4e6")
+  });
+
+  $("#text").click(function () {
+    $(".app").hide()
+    $(".graphics").hide()
+    $(".data").hide()
+    $(".text").show()
+    $(".docs").hide()
+    $(".btn.reset span").css("background-color", "#f6f4e6")
+  });
+
+  $("#app").click(function () {
+    $(".text").hide()
+    $(".graphics").hide()
+    $(".data").hide()
+    $(".app").show()
+    $(".docs").hide()
+    $(".btn.reset span").css("background-color", "#f6f4e6")
+  });
+
+  $("#docs").click(function () {
+    $(".text").hide()
+    $(".graphics").hide()
+    $(".data").hide()
+    $(".app").hide()
+    $(".docs").show()
+    $(".btn.reset span").css("background-color", "#f6f4e6")
+  });
+
+  $("#reset").click(function () {
+    $(".text").show()
+    $(".graphics").show()
+    $(".data").show()
+    $(".app").show()
+    $(".docs").show()
+    $(".btn.reset span").css("background-color", "#fddb3a")
+  });
+})
+
 root.render(
   <React.StrictMode>
     <App />
