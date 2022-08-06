aaceec20f286441e3841882b3f0f3dd1e6399879
Aadit Tambe <aadit.tambe@gmail.com>
2022-08-06 01:26:19 -0400

    add body tag

--- src/components/Experience.js
+++ src/components/Experience.js
@@ -67,63 +67,65 @@ function Experience() {
   })
 
   return (
-    <div className="experience">
-      <div className="intro">
-        <h1>🖥 Projects</h1>
-        <p>I specialize in telling data-driven stories visually, and my work helps people understand the news and make sense of the policies that impact them.</p>
-        <p>This page includes projects I have worked on for <a href="https://www.washingtonpost.com/">The Washington Post</a>, <a href="https://merrill.umd.edu/howard-center-for-investigative-journalism">the Howard Center for Investigative Journalism</a>, <a href="https://cnsmaryland.org/">Capital News Service</a>, <a href="https://www.nbcnews.com/datagraphics">NBC News</a>, <a href="https://dailyiowan.com/">The Daily Iowan</a>, as well as personal practice projects. </p>
-      </div>
-      <fieldset className="btn-row">
-        <legend>
-          <p> 👀 Looking for a particular type of project? </p>
-        </legend>
-        <p className="filters">
-          <label className="btn reset">
-            <input type="radio" name="storyType" value="column" id="reset" />
-            <span>🌎<br></br>All</span>
-          </label>
-          <label className="btn">
-            <input type="radio" name="storyType" value="text" id="text" />
-            <span>✍️<br></br>Written</span>
-          </label>
-          <label className="btn">
-            <input type="radio" name="storyType" value="data" id="data" />
-            <span>📈<br></br>Data-driven</span>
-          </label>
-          <label className="btn">
-            <input type="radio" name="storyType" value="graphics" id="graphics" />
-            <span>🗺<br></br>Graphics</span>
-          </label>
-          <label className="btn">
-            <input type="radio" name="storyType" value="app" id="app" />
-            <span>🖥<br></br>News app</span>
-          </label>
-          <label className="btn">
-            <input type="radio" name="storyType" value="app" id="docs" />
-            <span>📖<br></br>Docs</span>
-          </label>
-        </p>
-      </fieldset>
-      <div className="layout">
-        {data.map((d, i) => (
-          <div className={`story ${d.storyType}`} key={i}>
-            <a href={d.url} rel="noreferrer" target="_blank">
-              <p className="story-tools">🧰 {d.tools}</p>
-              <img
-                src={d.img.startsWith("https") ? d.img : require(`../images/${d.img}?.jpeg`)}
-                alt={d.alt}
-                className="story-img"
-              ></img>
-              <p className="story-org">📍<span className="ital"> {d.org}</span></p>
-              <p>🔗 <span className="story-name">{d.project}</span></p>
+    <body>
+      <div className="experience">
+        <div className="intro">
+          <h1>🖥 Projects</h1>
+          <p>I specialize in telling data-driven stories visually, and my work helps people understand the news and make sense of the policies that impact them.</p>
+          <p>This page includes projects I have worked on for <a href="https://www.washingtonpost.com/">The Washington Post</a>, <a href="https://merrill.umd.edu/howard-center-for-investigative-journalism">the Howard Center for Investigative Journalism</a>, <a href="https://cnsmaryland.org/">Capital News Service</a>, <a href="https://www.nbcnews.com/datagraphics">NBC News</a>, <a href="https://dailyiowan.com/">The Daily Iowan</a>, as well as personal practice projects. </p>
+        </div>
+        <fieldset className="btn-row">
+          <legend>
+            <p> 👀 Looking for a particular type of project? </p>
+          </legend>
+          <p className="filters">
+            <label className="btn reset">
+              <input type="radio" name="storyType" value="column" id="reset" />
+              <span>🌎<br></br>All</span>
+            </label>
+            <label className="btn">
+              <input type="radio" name="storyType" value="text" id="text" />
+              <span>✍️<br></br>Written</span>
+            </label>
+            <label className="btn">
+              <input type="radio" name="storyType" value="data" id="data" />
+              <span>📈<br></br>Data-driven</span>
+            </label>
+            <label className="btn">
+              <input type="radio" name="storyType" value="graphics" id="graphics" />
+              <span>🗺<br></br>Graphics</span>
+            </label>
+            <label className="btn">
+              <input type="radio" name="storyType" value="app" id="app" />
+              <span>🖥<br></br>News app</span>
+            </label>
+            <label className="btn">
+              <input type="radio" name="storyType" value="app" id="docs" />
+              <span>📖<br></br>Docs</span>
+            </label>
+          </p>
+        </fieldset>
+        <div className="layout">
+          {data.map((d, i) => (
+            <div className={`story ${d.storyType}`} key={i}>
+              <a href={d.url} rel="noreferrer" target="_blank">
+                <p className="story-tools">🧰 {d.tools}</p>
+                <img
+                  src={d.img.startsWith("https") ? d.img : require(`../images/${d.img}?.jpeg`)}
+                  alt={d.alt}
+                  className="story-img"
+                ></img>
+                <p className="story-org">📍<span className="ital"> {d.org}</span></p>
+                <p>🔗 <span className="story-name">{d.project}</span></p>
 
-            </a>
-          </div>
-        ))}
-      </div>
-      <div className="source ital"><p>I am a supporter of open-source code — the source code for this website is available on <a href="https://github.com/aadittambe/dot-com-two-point-o">GitHub</a>.</p></div>
-    </div >
-  );
+              </a>
+            </div>
+          ))}
+        </div>
+        <div className="source ital"><p>I am a supporter of open-source code — the source code for this website is available on <a href="https://github.com/aadittambe/dot-com-two-point-o">GitHub</a>.</p></div>
+      </div >
+    </body>
+  )
 }
 
 export default Experience;
--- src/components/Resume.js
+++ src/components/Resume.js
@@ -10,60 +10,62 @@ const skills = data.skills
 // console.log(skills)
 function Resume() {
     return (
-        <div className="resume">
-            <h1>📝 Resume</h1>
-            <div className="ital source">
-                <p >And <a href={Pdf} rel="noreferrer" target="_blank">here's</a> a PDF, if that's what you're looking for.</p>
-            </div>
-            <div className="section">
-                <h2>💼 Work experience</h2>
-                {work.map((d, i) => (
-                    <div className="item" key={i}>
-                        <h3 className="org">{d.org}</h3>
-                        <div className="exp">
-                            <div className="left">
-                                <h4 className="dates">{d.dates}</h4>
-                            </div>
-                            <div className="right">
-                                <h4 className="title">{d.title}</h4>
-                                <p className="desc">{d.desc}</p>
+        <body>
+            <div className="resume">
+                <h1>📝 Resume</h1>
+                <div className="ital source">
+                    <p >And <a href={Pdf} rel="noreferrer" target="_blank">here's</a> a PDF, if that's what you're looking for.</p>
+                </div>
+                <div className="section">
+                    <h2>💼 Work experience</h2>
+                    {work.map((d, i) => (
+                        <div className="item" key={i}>
+                            <h3 className="org">{d.org}</h3>
+                            <div className="exp">
+                                <div className="left">
+                                    <h4 className="dates">{d.dates}</h4>
+                                </div>
+                                <div className="right">
+                                    <h4 className="title">{d.title}</h4>
+                                    <p className="desc">{d.desc}</p>
+                                </div>
                             </div>
                         </div>
-                    </div>
-                ))}
-            </div>
-            <div className="section skls">
-                <h2>🛠 Skills</h2>
-                <div className="skills">
-                    {skills.map((d, i) => (
+                    ))}
+                </div>
+                <div className="section skls">
+                    <h2>🛠 Skills</h2>
+                    <div className="skills">
+                        {skills.map((d, i) => (
 
-                        <div className="skill">
-                            <h3>{d.name}</h3>
-                            {d.tools.map((skills, i) => (
-                                <p>{skills}</p>
-                            ))}
+                            <div className="skill">
+                                <h3>{d.name}</h3>
+                                {d.tools.map((skills, i) => (
+                                    <p>{skills}</p>
+                                ))}
+                            </div>
+                        ))}
+                    </div>
+                </div>
+                <div className="section">
+                    <h2>📚 Education</h2>
+                    {ed.map((d, i) => (
+                        <div className="item" key={i}>
+                            <h3 className="org">{d.school}</h3>
+                            <div className="exp">
+                                <div className="left">
+                                    <h4 className="dates">{d.year}</h4>
+                                </div>
+                                <div className="right">
+                                    <h4 className="title">{d.degree}, {d.focus}</h4>
+                                    {/* <p className="desc">{d.focus}</p> */}
+                                </div>
+                            </div>
                         </div>
                     ))}
                 </div>
             </div>
-            <div className="section">
-                <h2>📚 Education</h2>
-                {ed.map((d, i) => (
-                    <div className="item" key={i}>
-                        <h3 className="org">{d.school}</h3>
-                        <div className="exp">
-                            <div className="left">
-                                <h4 className="dates">{d.year}</h4>
-                            </div>
-                            <div className="right">
-                                <h4 className="title">{d.degree}, {d.focus}</h4>
-                                {/* <p className="desc">{d.focus}</p> */}
-                            </div>
-                        </div>
-                    </div>
-                ))}
-            </div>
-        </div>
+        </body>
     )
 }
 
