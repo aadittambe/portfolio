4b0300117fd5cd3c5980a0a908adb6de77664f97
Aadit Tambe <aadit.tambe@gmail.com>
2022-09-07 00:27:17 -0400

    refactor: break sparklines into its own component

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,82 +1,16 @@
 import React from 'react'
-import * as d3 from 'd3'
+
 import '../styles/Hero.css'
 import TypeIt from "typeit-react";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faEnvelope } from '@fortawesome/free-solid-svg-icons'
 import { faTwitter, faGithubAlt, faLinkedin } from '@fortawesome/free-brands-svg-icons'
 
-function Hero() {
-    d3.json("https://raw.githubusercontent.com/aadittambe/dot-com-two-point-o/main/gh-contribs/contribs.json").then(function (json) {
-        // console.log(json);
-        var content = json.data.user.contributionsCollection.contributionCalendar.weeks
-        // console.log(content);
-        let data = {}
-        content.map(d => {
-            const startDate = d.firstDay
-            const l1 = d.contributionDays
-            // console.log(l1);
-            let weekList = 0;
-            l1.map(l => {
-                weekList = weekList + l.contributionCount
-                return weekList
-            })
-            data[startDate] = weekList
-            return data
-        })
-        data = Object.values(data).map(d => +d);
-        // console.log(data);
-        const width = 100;
-        const height = 20;
-        const margin = { top: 2, right: 2, bottom: 2, left: 2 };
-        const boundedwidth = width - margin.left - margin.right;
-        const boundedheight = height - margin.top - margin.bottom;
 
-        const xScale = d3
-            .scaleLinear()
-            .domain([0, data.length])
-            .range([0, boundedwidth]);
-        const yScale = d3
-            .scaleLinear()
-            .domain([0, d3.max(data)])
-            .range([boundedheight, 0]);
+import Sparklines from '../components/Sparklines'
 
-        const svg = d3
-            .select("#sparklines")
-            .append("svg")
-            .attr("width", width)
-            .attr("height", height)
-            .append("g")
-            .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+const Hero = props => {
 
-        const line = d3
-            .line()
-            .x((d, i) => xScale(i))
-            .y((d) => yScale(d));
-
-        svg
-            .append("path")
-            .datum(data)
-            .attr("fill", "none")
-            .attr("stroke", "#f6f4e6")
-            .attr("stroke-width", 1)
-            .attr("d", line);
-
-        svg
-            .append("circle")
-            .attr("r", 2)
-            .attr("cx", xScale(0))
-            .attr("cy", yScale(data[0]))
-            .attr("fill", "#f6f4e6");
-
-        svg
-            .append("circle")
-            .attr("r", 2)
-            .attr("cx", xScale(data.length - 1))
-            .attr("cy", yScale(data[data.length - 1]))
-            .attr("fill", "#edcf2e");
-        return json
-    })
     return (
 
         <main className='hero'>
@@ -107,8 +41,9 @@ function Hero() {
                         return instance;
                     }}
                 /></span></p>
-            <p>Simply put, I am a journalist and a programmer. I use my skills to identify data-driven stories, and present them in visual ways. </p>
-            <p>Currently, I work on the news design team at The Washington Post. Previosuly, I was a data reporting fellow at the Howard Center for Investigative Journalism at the University of Maryland, and have interned at NBC News on the data and graphics team. My code contributions on GitHub in the last year look like this <span id='sparklines'></span>.</p>
+            <p>Currently, I work on the news design team at The Washington Post, where I build rich, highly customized interactive storytelling experiences, web apps and digital products.</p>
+
+            <p>Previosuly, I was a data reporting fellow at the Howard Center for Investigative Journalism at the University of Maryland, and have interned at NBC News on the data and graphics team. My code contributions on GitHub in the last year look like this <Sparklines data="https://raw.githubusercontent.com/aadittambe/dot-com-two-point-o/main/gh-contribs/contribs.json" />.</p>
             <p>If you like to keep up with current events and care about the news — or want to simply chat about trains or nerd out about antique fountain pens — we should connect!</p>
             <a href='https://twitter.com/aadittambe/' rel="noreferrer" target="_blank">
                 <FontAwesomeIcon className="icon" icon={faTwitter} size='2x' />
---
+++ src/components/Sparklines.js
@@ -0,0 +1,80 @@
+import React from 'react';
+import * as d3 from 'd3'
+import '../styles/Hero.css'
+
+const Sparklines = props => {
+    const { data } = props
+    d3.json(data).then(function (json) {
+        // console.log(json);
+        var content = json.data.user.contributionsCollection.contributionCalendar.weeks
+        // console.log(content);
+        let data = {}
+        content.map(d => {
+            const startDate = d.firstDay
+            const l1 = d.contributionDays
+            // console.log(l1);
+            let weekList = 0;
+            l1.map(l => {
+                weekList = weekList + l.contributionCount
+                return weekList
+            })
+            data[startDate] = weekList
+            return data
+        })
+        data = Object.values(data).map(d => +d);
+        // console.log(data);
+        const width = 100;
+        const height = 20;
+        const margin = { top: 2, right: 2, bottom: 2, left: 2 };
+        const boundedwidth = width - margin.left - margin.right;
+        const boundedheight = height - margin.top - margin.bottom;
+
+        const xScale = d3
+            .scaleLinear()
+            .domain([0, data.length])
+            .range([0, boundedwidth]);
+        const yScale = d3
+            .scaleLinear()
+            .domain([0, d3.max(data)])
+            .range([boundedheight, 0]);
+
+        const svg = d3
+            .select("#sparklines")
+            .append("svg")
+            .attr("width", width)
+            .attr("height", height)
+            .append("g")
+            .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+
+        const line = d3
+            .line()
+            .x((d, i) => xScale(i))
+            .y((d) => yScale(d));
+
+        svg
+            .append("path")
+            .datum(data)
+            .attr("fill", "none")
+            .attr("stroke", "#f6f4e6")
+            .attr("stroke-width", 1)
+            .attr("d", line);
+
+        svg
+            .append("circle")
+            .attr("r", 2)
+            .attr("cx", xScale(0))
+            .attr("cy", yScale(data[0]))
+            .attr("fill", "#f6f4e6");
+
+        svg
+            .append("circle")
+            .attr("r", 2)
+            .attr("cx", xScale(data.length - 1))
+            .attr("cy", yScale(data[data.length - 1]))
+            .attr("fill", "#edcf2e");
+        return json
+    })
+    return (<span id='sparklines'></span>)
+}
+
+export default Sparklines;
