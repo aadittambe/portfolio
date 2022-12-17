18859f6abb43ed16b6844e8ddb338f22973dc3ad
Aadit Tambe <aadit.tambe@gmail.com>
2022-12-17 00:37:17 -0500

    add deploy script

---
+++ scripts/deploy.js
@@ -0,0 +1,31 @@
+const FtpDeploy = require("ftp-deploy");
+const ftpDeploy = new FtpDeploy();
+require('dotenv').config();
+
+
+if (process.env.NODE_ENV !== 'production') {
+    require('dotenv').config();
+}
+console.log(process.env.FOO)
+
+
+const config = {
+    user: process.env.FTP_USERNAME,
+    password: process.env.FTP_PASSWORD,
+    host: "aadittambe.com",
+    port: 21, // Your credentials
+
+    localRoot: 'build/', // Location of build files in project
+    remoteRoot: "public_html/", // Upload location on remote, replace with subfolder on FTP-server if required
+
+    include: ["*", "**/*"], // Upload all files from build folder
+    exclude: [], // Exclude no files
+
+    deleteRemote: false, // Set to true if you want to delete ALL FILES in the remote root before uploading
+    forcePasv: true // Use passive mode
+};
+
+ftpDeploy
+    .deploy(config)
+    .then((res) => console.log("finished:", res))
+    .catch((err) => console.log(err));
